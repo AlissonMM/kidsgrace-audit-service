4bd341d6732704fb6c5158ec01502c86dc4e47df
AlissonMM <alissonmmedeji@gmail.com>
2026-08-03 18:32:05 -0300

    feat: Add "Blocking" annotation to consume method in AuditConsumer

--- src/main/java/edu/meialua/kafka/consumer/AuditConsumer.java
+++ src/main/java/edu/meialua/kafka/consumer/AuditConsumer.java
@@ -1,6 +1,7 @@
 package edu.meialua.kafka.consumer;
 
 import edu.meialua.dto.LogEvent;
+import io.smallrye.common.annotation.Blocking;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
@@ -11,6 +12,10 @@ public class AuditConsumer {
     // that is configured to listen to the "logs" topic in kafka
     // in the application.properties
     @Incoming("audit")
+    // Moves the execution from the reactive event loop to a worker thread,
+    // allowing blocking operations (e.g., database access) without blocking
+    // the processing of other incoming Kafka messages.
+    @Blocking
     public void consume(LogEvent logEvent) {
         System.out.println("Event Received: ");
         System.out.println(logEvent);
